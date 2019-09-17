86c5228241be7ad3b3e8f3ac08494a06e922e19c
Taylor Jordan <taylornoel418@icloud.com>
2019-09-17 13:13:52 -0600

    Add photo component to render data

---
+++ src/components/Photo.vue
@@ -0,0 +1,21 @@
+<template>
+  <img :src='photoPath' :alt='photo.alt_description'/>
+</template>
+
+<script>
+    export default {
+        props: ['photo'],
+        computed: {
+            photoPath: function() {
+                return this.photo.urls.small
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    img {
+        box-shadow: 0 0 35px black;
+        margin: 0 8px 8px 0;
+    }
+</style>
--- src/components/PhotosContainer.vue
+++ src/components/PhotosContainer.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
-
-    </div>
+    <ul>
+        <li :key='photo.id' v-for='photo in photos'>
+            <Photo :photo='photo' />
+        </li>
+    </ul>
 </template>
 
 <script>
     import { apiKey } from '../../utils/apiKey.js';
+    import Photo from './Photo.vue'
     export default {
         name: 'PhotosContainer',
         data() {
@@ -20,7 +23,7 @@
         methods: {
             fetchData: async function() {
                 try {
-                    const url = `https://api.unsplash.com/photos/?client_id=`
+                    const url = `https://api.unsplash.com/photos/?client_id=${apiKey}`
                     const response = await fetch(url)
                     const photos = await response.json()
 
@@ -30,10 +33,30 @@
                     this.error = message
                 }
             }
+        },
+        components: {
+            Photo
         }
     }
 </script>
 
 <style scoped>
-
+    ul {
+        display: flex;
+        flex-flow: column wrap;
+        flex-wrap: wrap;
+        list-style: none;
+        max-height: 1000px;
+        margin-left: -8px; 
+        overflow: hidden;
+        width: 100%;
+        /* display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        list-style: none;
+        margin: 0;
+        padding: 1rem; 
+        /* grid-row-gap: 1rem; */
+        /* grid-template-columns: repeat(6, 1fr) */
+    }
 </style>
