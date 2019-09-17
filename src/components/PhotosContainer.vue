8892a50b8f8855a896fb9e221adcf05695de39b6
Taylor Jordan <taylornoel418@icloud.com>
2019-09-17 06:39:58 -0600

    Add photo container and initial state

---
+++ src/components/PhotosContainer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PhotosContainer',
+        data() {
+            return {
+                photos: []
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
